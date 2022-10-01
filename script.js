add78fa75726d02c7319e74ba74423a48f3fe9a9
Gozie <87874051+itsgozie@users.noreply.github.com>
2022-10-01 22:51:06 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Arrays and Objects
+const bioData = [{
+    Curry: {
+      firstName: 'Stephen',
+      lastName: 'Curry',
+      franchise: 'Warriors',
+      season: 12,
+      record: {
+        championship: 4,
+        mvp: 1,
+        finalsMvp: 1,
+
+      },
+    }
+  },
+  {
+    Lebron: {
+      firstName: 'Lebron',
+      lastName: 'James',
+      franchise: 'Lakers',
+      season: 20,
+      record: {
+        championship: 4,
+        mvp: 1,
+        finalsMvp: 4,
+
+      },
+    }
+  }
+];
